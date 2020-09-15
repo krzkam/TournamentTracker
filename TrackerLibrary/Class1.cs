3b239f96ab7b218aa9230a1ed65f26011a6f8f2e
KAMINKR1 <KAMINKR1@PSTM17741L>
2020-09-15 15:49:09 +0200

    20200915

--- TrackerLibrary/Class1.cs
+++ TrackerLibrary/Class1.cs
@@ -4,6 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Tournament tracker
+
+//Technology:
+//	Multi-form WinForms application
+
+//    In-depth Class Library
+//    SQL Database
+//    Text File Storage
+
+//    Dapper
+//    Linq
+
+//    Interfaces
+//    Emailing from C#
+//	Custom Events
+
+//    Advanced Debugging
+
+//    More...
+
 namespace TrackerLibrary
 {
     public class Class1
