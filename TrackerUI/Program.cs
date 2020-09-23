b3fe15e7733fc5627172edf1f58619387bb9a9de
KAMINKR1 <KAMINKR1@PSTM17741L>
2020-09-23 12:59:35 +0200

    20200923

--- TrackerUI/Program.cs
+++ TrackerUI/Program.cs
@@ -17,6 +17,6 @@ namespace TrackerUI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new TournamentDashboardForm());
-        }
+        } 
     }
 }
