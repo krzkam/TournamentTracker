45e8e7acf5be03b1d6551810d879b8edb0760a0f
KAMINKR1 <KAMINKR1@PSTM17741L>
2020-10-26 13:48:07 +0100

    20201026

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -4,15 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelpers;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrizeModels.csv";
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();//Load the text file & Convert the text to List<PrizeModel>
 
+            int currentId = 1;
+            if (prizes.Count > 0)
+            {
+                currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1; //Find the max ID
+            }
+            model.Id = currentId;
+            prizes.Add(model); //Add the new record with the new ID
+            //Convert the prizes to list<string>
+            //Save the list<string> to the text file
+            prizes.SaveToPrizeFile(PrizesFile);
             return model;
         }
     }
