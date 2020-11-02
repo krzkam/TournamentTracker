f6b66dd6eaaf6f1a3c2c9967ee317e647cd888d5
Krzysztof Kamiński <krzkaminski@wp.pl>
2020-11-02 19:51:16 +0100

    20201102

--- TrackerLibrary/Models/TeamModel.cs
+++ TrackerLibrary/Models/TeamModel.cs
@@ -39,6 +39,7 @@ namespace TrackerLibrary.Models
 {
     public class TeamModel
     {
+        public int Id { get; set; }
         public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
         public string TeamName  { get; set; }
 
--- TrackerUI/CreateTeamForm.cs
+++ TrackerUI/CreateTeamForm.cs
@@ -103,5 +103,14 @@ namespace TrackerUI
                 WireUpList(); 
             }
         }
+
+        private void createTeamButton_Click(object sender, EventArgs e)
+        {
+            TeamModel t = new TeamModel();
+            t.TeamName = teamNameValue.Text;
+            t.TeamMembers = selectedTeamMembers;
+            t = GlobalConfig.Connection.CreateTeam(t);
+            //TODO - If we aren't closing this form after creation,reset the form
+        }
     }
 }
